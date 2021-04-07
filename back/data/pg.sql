0cf6116189f432328e5140d771e3866f41cd10d1
quentin_macq <quentin_macq@ens.univ-artois.fr>
2021-04-07 09:26:47 +0200

    Modif pg.sql

--- back/data/pg.sql
+++ back/data/pg.sql
@@ -19,16 +19,15 @@ values ('histoire'),
 
 create table quiz
 (
-    quiz_id     serial primary key,
-    key_word_id int,
-    name        varchar not null,
-    image       varchar not null,
-    foreign key (key_word_id) references key_word (key_word_id)
+    quiz_id  serial primary key,
+    name     varchar not null,
+    image    varchar not null,
+    key_word varchar not null
 );
 
-insert into quiz(name, image, key_word_id)
-values ('Animals', 'https://picsum.photos/200/100', 5),
-       ('Technology', 'https://picsum.photos/200', 3);
+insert into quiz(name, image, key_word)
+values ('Animals', 'https://picsum.photos/200/100', 'animal'),
+       ('Technology', 'https://picsum.photos/200', 'technology');
 
 create table question
 (
@@ -46,12 +45,15 @@ create table question
 );
 
 insert into question(sentence, image, answer_1, answer_2, answer_3, answer_4, nb_points, quiz_id)
-values ('What is the slowest animal of the world ?', 'https://picsum.photos/200', 'Three-toed Sloth', 'Four-toed Sloth', 'Three-toed Turtle', 'Four-toed Turtle', 1, 1),
+values ('What is the slowest animal of the world ?', 'https://picsum.photos/200', 'Three-toed Sloth', 'Four-toed Sloth',
+        'Three-toed Turtle', 'Four-toed Turtle', 1, 1),
        ('Which bird is a universal symbol of peace ?', null, 'Eagle', 'Magpie', 'Swallow', 'Dove', 2, 1),
        ('Which animal has the highest blood pressure ?', null, 'Horse', 'Sloth', 'Turtle', 'Giraffe', 3, 1),
        ('The python is a poisonous snake. True or false ?', null, 'True', 'False', null, null, 3, 1),
-       ('Which of these is not a peripheral, in computer terms ?', null, 'Keyboard', 'Motherboard', 'Mouse', 'Monitor', 3, 2),
-       ('A network designed to allow communication within an organization is called ?', null, 'The World Wide Web', 'Yahoo', 'An intranet', 'The Internet', 2, 2),
+       ('Which of these is not a peripheral, in computer terms ?', null, 'Keyboard', 'Motherboard', 'Mouse', 'Monitor',
+        3, 2),
+       ('A network designed to allow communication within an organization is called ?', null, 'The World Wide Web',
+        'Yahoo', 'An intranet', 'The Internet', 2, 2),
        ('When was the DVD introduced ?', null, '1995', '1990', '2000', '1970', 3, 2);
 
 create table player
